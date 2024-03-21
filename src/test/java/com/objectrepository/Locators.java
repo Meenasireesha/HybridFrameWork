15337a79a98e428d7ecdf3634f840b212e2dea89
PawanKS <PawanKS@DESKTOP-82M7FQF>
2024-03-21 20:44:57 +0530

    Hybrid Frame work program with the refresha nd invalid data

--- src/test/java/com/objectrepository/Locators.java
+++ src/test/java/com/objectrepository/Locators.java
@@ -13,5 +13,11 @@ public class Locators {
 	public final By podlogin_contacttype_dropdown1=By.xpath("//*[@id=\"content\"]/div/div[2]/div[2]/form/div[4]/div/div/div/input");
 	public final By podlogin_contacttype_dropdown=By.xpath("/html/body/div[2]/div[1]/div[1]/ul/li[1]");
 	public final By podlogin_submit_button=By.xpath("//*[@id=\"content\"]/div/div[2]/div[2]/form/button");
+	public final By podlogin_login_click1=By.xpath("//*[@id=\"app\"]/div[1]/div/div[4]/div/div[1]/a[1]");
+	public final By podlogin_email_editbox=By.name("email");
+	public final By podlogin_password_editbox=By.name("password");
+	public final By podlogin_contact_click=By.xpath("//*[@id=\"content\"]/div/div[2]/form/div[4]/div/div/label[1]/span");
+	public final By podlogin_login_click=By.xpath("//*[@id=\"content\"]/div/div[2]/form/div[5]/div/button");
+	
 	
 }
--- src/test/java/com/testscenarios/podfood_meena.java
+++ src/test/java/com/testscenarios/podfood_meena.java
@@ -22,6 +22,26 @@ public class podfood_meena {
 		Thread.sleep(2000);
 		driver.findElement(loc.podlogin_contacttype_dropdown).click();
 		driver.findElement(loc.podlogin_submit_button).click();
+//		refresh the data and click on login button without the data and refresh.
+//		give invalid data and click on login button and error messages sould display in console. 
+		driver.navigate().refresh();
+		driver.findElement(loc.podlogin_login_click1).click();
+//		driver.findElement(loc.fblogin_email_editbox).sendKeys(")
+		driver.findElement(loc.podlogin_login_click).click();
+		String a = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/form/div[2]/div/div[2]")).getText();
+		System.out.println(a);
+		String b= driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/form/div[3]/div/div[2]")).getText();
+		System.out.println(b);
+		String c= driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/form/div[4]/div/div[2]")).getTagName();
+		System.out.println(c);
+		driver.navigate().refresh();
+//		Thread.sleep(2000);
+		driver.findElement(loc.podlogin_email_editbox).sendKeys("gdshjedh");
+		driver.findElement(loc.podlogin_password_editbox).sendKeys("7362879");
+		driver.findElement(loc.podlogin_contact_click).click();
+		driver.findElement(loc.podlogin_login_click).click();
+		
+		
 	}
 
 }
